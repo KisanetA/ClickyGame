a4cc1560d4d4621d8e5ffda2b7226cc88a03af8d
Kisanet Asfaha <kisanetasfaha@gmail.com>
2019-09-21 16:17:55 -0400

    added components

---
+++ src/components/Navbar/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./style.css";
+
+function Title(props) {
+  <div>
+    <nav class="navbar">
+      <ul>
+        <li class="brand">
+          <a href="/"> Clicky Game</a>
+        </li>
+      </ul>
+    </nav>
+  </div>
+}
+
+export default Title;
